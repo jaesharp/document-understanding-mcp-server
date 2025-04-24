ccf53d557f8323bce62884c1e206fb4953f1f798
J <j@jaesharp.com>
2025-04-25 00:17:49 +1000

    Unneeded/incorporated into tooling

--- fix_imports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source .venv/bin/activate
-
-# Run autoflake on all Python files in src and tests directories
-autoflake --in-place --remove-all-unused-imports --recursive src/ tests/
-
-echo "Unused imports removed successfully!"
